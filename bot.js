6b360fdc0fa97a4dd840fbd1198b31c8bb9e89c5
hahahaezGG <43311837+hahahaezGG@users.noreply.github.com>
2018-09-29 22:30:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const log = message => {
 };
 
 client.on("ready", () => {
-  client.user.setGame(prefix + "ABK Bot Yeni Versiyon") 
+  client.user.setGame(prefix + "yardım | BLYBot | 7-24 | ONLINE") 
   console.log("Bağlandım!")   
 });
 
